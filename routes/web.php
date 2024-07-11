4d503f524bc19dc72920a1b68b4a8ec8da86bd09
GeovanniMarrocos <Geovannipessoal@outlook.com>
2024-07-11 15:29:31 -0300

    upgrade files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    UserController
+};
+
+use Illuminate\Support\Facades\{
+    Route
+};
 
 
 Route::get('/users', [UserController::class])->name('users.index');
