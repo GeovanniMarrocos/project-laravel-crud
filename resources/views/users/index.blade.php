b4eb8ead426d48cb99e74cf46a47fcc2a6f72a4f
GeovanniMarrocos <Geovannipessoal@outlook.com>
2024-07-12 10:44:25 -0300

    upgrade files

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,8 +6,13 @@
     <title>Document</title>
 </head>
 <body>
-    <div>
-        <p>Listagem de Usuários</p>
-    </div>
+    <h1>Listagem dos usuários</h1>
+    <ul>
+        @foreach ($users as $user)
+            <li>
+                {{$user->name}} - {{$user->email}}
+            </li>
+        @endforeach
+    </ul>
 </body>
 </html>
