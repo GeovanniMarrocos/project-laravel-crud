e812b2b7823375cff484d0f639376f60e647f7a2
GeovanniMarrocos <Geovannipessoal@outlook.com>
2024-07-12 22:20:14 -0300

    upgrade files

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,6 +6,7 @@
 <h1>Novo Usuário</h1>
 
 <form action="{{ route('users.store') }}" method="post">
+    @csrf
     <input type="text" name="name" placeholder="Nome do Usuário:" required>
     <input type="email" name="email" placeholder="E-mail:" required>
     <input type="password" name="password" placeholder="Senha:" required>
