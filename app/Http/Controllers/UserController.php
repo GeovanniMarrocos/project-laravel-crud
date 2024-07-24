984c219c86d799e3b5bc9adcb739383f749b9f29
GeovanniMarrocos <Geovannipessoal@outlook.com>
2024-07-23 23:30:47 -0300

    update file

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,14 +19,11 @@ class UserController extends Controller
     public function show($id)
     {
 
-        if(!$user = User::find($id))
-        {
+        if (!$user = User::find($id)) {
             return redirect()->route('users.index');
-        }else
-        {
+        } else {
             return view('users.show', compact('user'));
         }
-
     }
 
     public function create()
@@ -42,36 +39,42 @@ class UserController extends Controller
         $user = User::create($data);
 
         return redirect()->route('users.index');
-
     }
 
     public function edit($id)
     {
-        if(!$user = User::find($id))
-        {
+        if (!$user = User::find($id)) {
             return redirect()->route('users.index');
         }
 
         return view('users.edit', compact('user'));
-
     }
 
     public function update(StoreUpdateUserFormRequest $request, $id)
     {
-        if(!$user = User::find($id))
-        {
+        if (!$user = User::find($id)) {
             return redirect()->route('users.index');
         }
 
         $data = $request->only('name', 'email');
 
-         if($request->password)
-           $data['password'] = bcrypt($request->password);
+        if ($request->password)
+            $data['password'] = bcrypt($request->password);
 
-          $user->update($data);
+        $user->update($data);
 
-          return redirect()->route('users.index');
-
+        return redirect()->route('users.index');
     }
 
+    public function destroy($id)
+    {
+
+        if (!$user = User::find($id))
+
+            return redirect()->route('users.index');
+
+        $user->delete();
+
+        return view('users.show', compact('user'));
+    }
 }
